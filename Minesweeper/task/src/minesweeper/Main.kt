510a29abb8938d0475cffec06e0d6a56086efa5c
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-03-09 02:25:53 +0200

    stage 5 completed

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -5,22 +5,19 @@ class MineSweeperGame(val numOfMines: Int = 10, val fieldWidth: Int = 10, val fi
     private val field = GameField(numOfMines, fieldWidth, fieldHeight)
     var isRunning = true
         private set
+    private var isFirstFreeCommand = true
 
     init {
         display()
         ask()
     }
 
-    private inner class GameField(val numOfMines: Int, val fieldWidth: Int, val fieldHeight: Int) {
-        val CLOSED: Int = 0
-        val OPENED: Int = 1
-        val MINED: Int = 2
-        val FIRED: Int = 3
-        val MARKED: Int = 4
+    private class GameField(val numOfMines: Int, val fieldWidth: Int, val fieldHeight: Int) {
+
         private val markedCells = mutableSetOf<Cell>()
         private val minedCells = mutableSetOf<Cell>()
 
-        val cells = List(fieldHeight) { List(fieldWidth) { Cell() } }
+        val cells = List(fieldHeight) { row -> List(fieldWidth) { col -> Cell(row, col) } }
         val positions = mutableSetOf<Int>()
 
         init {
@@ -31,66 +28,184 @@ class MineSweeperGame(val numOfMines: Int = 10, val fieldWidth: Int = 10, val fi
 //            println(positions)
             positions.forEach { addMine(it / fieldHeight, it % fieldWidth) }
 
+            minedCells.forEach {  println(it.summary()) }
 
         }
 
-        private inner class Cell(state: Int = CLOSED) {
-            var state = state
-                private set
-            private var nearHowManyMines = 0
+        private class Cell(val row: Int, val col: Int) {
+            val cellId: Int
 
-            fun open(): Boolean {
-                if (state == CLOSED) {
-                    state = OPENED
-                    return true
-                } else if (state == MINED) {
-                    state = FIRED
-                    return true
+            enum class OpenState { OPENED, CLOSED }
+            enum class MarkState { MARKED, UNMARKED }
+            enum class MineState { MINED, UNMINED, FIRED }
+
+            companion object {
+                val CLOSED: Int = 0
+                val OPENED: Int = 1
+                val MINED: Int = 2
+                val FIRED: Int = 3
+                val MARKED: Int = 4
+
+                var id = 0
+            }
+
+            init {
+                cellId = id++
+            }
+
+            var openState = OpenState.CLOSED
+                private set
+            var markState = MarkState.UNMARKED
+                private set
+            var mineState = MineState.UNMINED
+                private set
+
+
+            private var nearHowManyMines = 0
+            private var nearHowManyMarked = 0
+
+            fun open() {
+                if (mineState == MineState.MINED) {
+                    mineState = MineState.FIRED
+//                    return true
                 }
-                return false
+                if (openState == OpenState.CLOSED) {
+                    openState = OpenState.OPENED
+//                    return true
+                }
+//                return false
             }
 
             fun addNearMine() {
                 nearHowManyMines++
             }
+            fun removeNearMine() {
+                nearHowManyMines--
+            }
+            fun addNearMarked() {
+                nearHowManyMarked++
+            }
+
+            fun removeNearMarked() {
+                nearHowManyMarked--
+            }
 
             override fun toString(): String {
-                return when (state) {
-                    OPENED -> ""
-                    CLOSED -> if (nearHowManyMines > 0) "$nearHowManyMines" else "."
-                    MINED -> "."
-                    FIRED -> "@"
-                    MARKED -> "*"
-                    else -> "!"
+                return when (openState) {
+                    OpenState.OPENED -> {
+                        when (mineState) {
+                            MineState.FIRED -> "X"
+                            else -> when (isNearMine()) {
+                                true -> nearHowManyMines.toString()
+                                false -> "/"
+                            }
+                        }
+//                        if (nearHowManyMines > 0) nearHowManyMines.toString() else "/"
+                    }
+
+                    else -> when (markState) {
+                        MarkState.MARKED -> "*"
+                        else -> "."
+                    }
+//                    CLOSED, MINED -> "."
+//                    FIRED -> "X"
+//                    MARKED -> "*"
+//                    else -> "!"
                 }
             }
 
-            fun putMine(): Boolean {
-                if (state == CLOSED) {
-                    state = MINED
-                    return true
-                }
-                return false
+            fun summary(): String {
+                return "row:${row + 1} ,col:${col + 1}, nearHowManyMarked:$nearHowManyMarked, nearHowManyMine:$nearHowManyMines"
             }
 
+            fun putMine() {
+//                if (state == CLOSED) {
+//                    state = MINED
+//                    return true
+//                }
+//                return false
+                mineState = MineState.MINED
+            }
+            fun removeMine() {
+                mineState = MineState.UNMINED
+            }
             fun toggleMark(): Boolean {
-                return if (state != MARKED) {
-                    state = MARKED
+                return if (markState == MarkState.UNMARKED) {
+                    markState = MarkState.MARKED
                     true
                 } else {
-                    state = CLOSED
+                    markState = MarkState.UNMARKED
                     false
                 }
             }
 
             fun isNearMine(): Boolean {
-                return nearHowManyMines > 0 && state != MINED
+                //state != MINED &&
+                return nearHowManyMines > 0 //&& state == OPENED
             }
 
+            fun isNearMarked(): Boolean {
+                return nearHowManyMarked > 0
+            }
+
+            fun isFired(): Boolean {
+                return mineState == MineState.FIRED
+            }
+
+            fun isClosed(): Boolean {
+                return openState == OpenState.CLOSED
+            }
+
+            fun isMinedOrMarked(): Boolean {
+                return mineState == MineState.MINED || markState == MarkState.MARKED
+            }
+
+            fun isMined(): Boolean {
+                return mineState == MineState.MINED //|| markState == MarkState.MARKED
+            }
+
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Cell
+
+                if (cellId != other.cellId) return false
+
+                return true
+            }
+
+            override fun hashCode(): Int {
+                return cellId
+            }
+
+            fun isOpen(): Boolean {
+                return openState == OpenState.OPENED
+            }
 
         }
 
+        fun MoveMine(col: Int, row: Int) {
+            removeMine( row,col)
+            var r = (0..fieldWidth * fieldHeight - 1).random()
+            while (r in positions) {
+                r = (0..fieldWidth * fieldHeight - 1).random()
+            }
+            positions.add(r)
+            println("new mine position: $r")
+            addMine(r / fieldHeight, r % fieldWidth)
+        }
 
+        fun removeMine(row: Int, col: Int) {
+
+            cells[row][col].removeMine()
+            minedCells.remove(cells[row][col])
+            val neighbors = getNeighboursOf(row, col)
+            neighbors.forEach {
+                it.removeNearMine()
+            }
+            positions.remove((row * col-1) + col)
+        }
         fun addMine(row: Int, col: Int) {
             cells[row][col].putMine()
             minedCells.add(cells[row][col])
@@ -129,6 +244,49 @@ class MineSweeperGame(val numOfMines: Int = 10, val fieldWidth: Int = 10, val fi
             return list
         }
 
+        private fun checkAndAddToSet(set: MutableSet<Cell>, c: Cell) {
+            if (!c.isMined() && c.isClosed())//&& !c.isNearMarked()
+                set.add(c)
+        }
+
+        private fun getNeighborsToBeOpened(row: Int, col: Int): Set<Cell> {
+            val set = mutableSetOf<Cell>()
+            if (row > 0) {
+                val c = cells[row - 1][col]
+                checkAndAddToSet(set, c)
+                if (col > 0) {
+                    val c = cells[row - 1][col - 1]
+                    checkAndAddToSet(set, c)
+                }
+                if (col < fieldWidth - 1) {
+                    val c = cells[row - 1][col + 1]
+                    checkAndAddToSet(set, c)
+                }
+            }
+            if (row < fieldHeight - 1) {
+                val c = cells[row + 1][col]
+                checkAndAddToSet(set, c)
+                if (col > 0) {
+                    val c = cells[row + 1][col - 1]
+                    checkAndAddToSet(set, c)
+                }
+                if (col < fieldWidth - 1) {
+                    val c = cells[row + 1][col + 1]
+                    checkAndAddToSet(set, c)
+                }
+            }
+            if (col > 0) {
+                val c = cells[row][col - 1]
+                checkAndAddToSet(set, c)
+            }
+            if (col < fieldWidth - 1) {
+                val c = cells[row][col + 1]
+                checkAndAddToSet(set, c)
+            }
+            return set
+        }
+
+
         fun draw() {
             val maxDigits = fieldHeight.toString().length
             println("${" ".repeat(maxDigits)}│${(1..fieldWidth).joinToString("")}│")
@@ -144,22 +302,71 @@ class MineSweeperGame(val numOfMines: Int = 10, val fieldWidth: Int = 10, val fi
             val marked = cells[row][col].toggleMark()
             if (marked) {
                 markedCells.add(cells[row][col])
+                val neighbors = getNeighboursOf(row, col)
+//                println("near marked")
+                neighbors.forEach {
+
+                    it.addNearMarked()
+//                    println(it.summary())
+                }
             } else {
                 markedCells.remove(cells[row][col])
+                val neighbors = getNeighboursOf(row, col)
+//                println("near unmarked")
+                neighbors.forEach {
+
+                    it.removeNearMarked()
+//                    println(it.summary())
+                }
             }
         }
 
-        fun isCellNearMine(col: Int, row: Int): Boolean {
-            return cells[row][col].isNearMine()
+        fun playerOpenCell(col: Int, row: Int) {
+            if (cells[row][col].isClosed()) {
+                cells[row][col].open()
+                if (!cells[row][col].isFired()) {
+                    val set = getNeighborsToBeOpened(row, col)
+
+                    set.forEach {
+//                        println(it.summary())
+                        openCell(it.col, it.row)
+                    }
+                }
+            }
+        }
+
+        private fun openCell(col: Int, row: Int) {
+            cells[row][col].open()
+            val set = getNeighborsToBeOpened(row, col)
+            set.forEach {
+//                println(it.summary())
+                openCell(it.col, it.row)
+            }
+        }
+
+        fun OpenAllMines() {
+            minedCells.forEach { it.open() }
+        }
+
+        fun isFiredCell(col: Int, row: Int): Boolean {
+            return cells[row][col].isFired()
+        }
+
+        fun isOpenAndNearMine(col: Int, row: Int): Boolean {
+            return cells[row][col].isNearMine() && cells[row][col].isOpen()
         }
 
         fun checkWin(): Boolean {
             return minedCells == markedCells
         }
+
+        fun mineWillBeFired(col: Int, row: Int): Boolean {
+            return cells[row][col].isMined()
+        }
     }
 
     private fun ask() {
-        print("Set/delete mine marks (x and y coordinates): ")
+        print("Set/unset mine marks or claim a cell as free: ")
 
     }
 
@@ -169,20 +376,46 @@ class MineSweeperGame(val numOfMines: Int = 10, val fieldWidth: Int = 10, val fi
     }
 
 
-    fun play(col: Int, row: Int): Boolean {
+    fun play(col: Int, row: Int, command: String): Boolean {
+        if (command == "mine") {
+            if (field.isOpenAndNearMine(col, row)) {
+                println("There is a number here!")
 
-        if (field.isCellNearMine(col, row)) {
-            println("There is a number here!")
+            } else {
 
-        } else {
+                field.toggleMarkCell(col, row)
+                display()
+                if (field.checkWin()) {
+                    println("Congratulations! You found all the mines!")
+                    isRunning = false
+                    return isRunning
+                }
 
-            field.toggleMarkCell(col, row)
-            display()
-            if (field.checkWin()) {
-                println("Congratulations! You found all the mines!")
-                isRunning = false
-                return isRunning
             }
+        } else { //free command
+
+            if (isFirstFreeCommand) {
+                if (field.mineWillBeFired(col, row)) {
+                    field.MoveMine(col, row)
+
+                }
+                field.playerOpenCell(col, row)
+                display()
+//                if (field.isFiredCell(col, row)) {
+//                    println("failed")
+//                }
+                isFirstFreeCommand = false
+            } else {
+                field.playerOpenCell(col, row)
+                if (field.isFiredCell(col, row)) {
+                    field.OpenAllMines()
+                    display()
+                    println("You stepped on a mine and failed!")
+                } else {
+                    display()
+                }
+            }
+
 
         }
         ask()
@@ -200,8 +433,8 @@ fun main() {
 
     while (game.isRunning) {
 
-        val (col, row) = readln().split(" ").map { it.toInt() }
-        game.play(col - 1, row - 1)
+        val (col, row, command) = readln().split(" ")
+        game.play(col.toInt() - 1, row.toInt() - 1, command)
     }
 
 }
